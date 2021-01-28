2be7fbb2c72e456e44a5379a306fa69245f0f9cd
mloebach <mloebach@ucsc.edu>
2021-01-27 21:51:31 -0800

    basic nani implementation

---
+++ PartyDetective/Assets/Scripts/TestCommand.cs
@@ -0,0 +1,24 @@
+﻿using Naninovel;
+using Naninovel.Commands;
+using UniRx.Async;
+using UnityEngine;
+
+[CommandAlias("testCommand")]
+public class TestCommand : Command
+{
+    public StringParameter Message;
+
+    public override UniTask ExecuteAsync (CancellationToken cancellationToken = default)
+    {
+        if (Assigned(Message))
+        {
+            Debug.Log($"Hello, {Message}.");
+        }
+        else
+        {
+            Debug.Log("Hello World!");
+        }
+
+        return UniTask.CompletedTask;
+    }
+}
